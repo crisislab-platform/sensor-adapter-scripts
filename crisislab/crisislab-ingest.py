e9196ce68e98452b99b8359337c0d3e123ff0afe
Zade Viggers <zade@viggers.net>
2024-01-08 09:50:09 +1300

    Comment & tidy up

--- crisislab/crisislab-ingest.py
+++ crisislab/crisislab-ingest.py
@@ -11,9 +11,9 @@ SERVER_PORT = ""
 
 while True:
     try:
-        print("opening udp")
+        print("Opening udp...")
         udp_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print("done with opening udp")
+        print("Done opening udp")
 
         def send_data_to_server(channel: str, data: list[int]):
             print("Sending data to server...")
@@ -23,35 +23,40 @@ while True:
             message_binary = formatted_message.encode('utf-8')
             udp_soc.sendto(message_binary, (SERVER_IP, SERVER_PORT))
 
-        print(f"connecting to serial port {SERIAL_PORT}")
+        print(f"Connecting to serial port {SERIAL_PORT}...")
         ser = serial.Serial(SERIAL_PORT, 9600)
-        print(f"done connecting to serial port")
+        print(f"Done connecting to serial port")
 
         lines_so_far = 0
         while True:
             line = ser.readline().decode('ascii')
-            print(f"Serial line:{line}")
+            print(f">{line}")
 
+            # Send dummy wifi name & password
             if lines_so_far < 2:
                 lines_so_far+=1
                 ser.write("no_wifi_pls".encode('ascii'))
 
+            # parse data packets
             try:
                 data = json.loads(line)
                 if "x" in data:
+                    # Crisis Lab X-axis
                     send_data_to_server("CLX", data["x"])
                     
                 if "y" in data:
+                    # Crisis Lab Y-axis
                     send_data_to_server("CLY", data["y"])
                     
                 if "z" in data:
+                    # Crisis Lab Z-axis
                     send_data_to_server("CLZ", data["z"])
                 
             except Exception as err:
                 print(err)
-                print("oh well. reading next line anyway")
+                print("Oh well. Reading next line anyway")
             
     except Exception as err:
         print(err)
-        print("oh well. starting over in 5s")
+        print("Oh well. Starting over in 5s")
         time.sleep(5)
