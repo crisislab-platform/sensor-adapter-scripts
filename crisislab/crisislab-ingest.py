3bee567765bc5b9306feffa9931fd6a5e085a16d
Zade Viggers <zade@viggers.net>
2024-05-03 12:58:11 +1200

    Update crisilab sensor adapter

--- crisislab/crisislab-ingest.py
+++ crisislab/crisislab-ingest.py
@@ -4,12 +4,18 @@ import json
 import socket
 
 # Change this to your serial port, e.g. COM3
-SERIAL_PORT = "/dev/ttyUSB0"
 
 SERVER_IP = "10.241.144.172"
 SERVER_PORT = 2098
 
-BAUD_RATE = 9600
+BAUD_RATE = 115200
+
+# We buffer ~100 data points before sending to the server
+buffers = {
+    "x": [],
+    "y": [],
+    "z": []
+}
 
 while True:
     try:
@@ -17,51 +23,48 @@ while True:
         udp_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print("Done opening udp")
 
-        def send_data_to_server(channel: str, data: list[int]):
+        def send_data_to_server(channel: str, data: list[str]):
             # Make it look like raspberry shake data
-            formatted_message = f"{{'{channel}', {str(time.time())}, {', '.join([str(s) for s in data])}}}"
+            formatted_message = f"{{'{channel}', {str(time.time())}, {', '.join(data)}}}"
             # Convert to binary
             message_binary = formatted_message.encode('utf-8')
             udp_soc.sendto(message_binary, (SERVER_IP, SERVER_PORT))
 
         print(f"Connecting to serial port {SERIAL_PORT}...")
-        # ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
-        # ser.close()
-        # print("Half way there...")
-        # # We need to close and reopen for the sensor to cooperate
-        # time.sleep(1)
+        ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+        ser.close()
+        print("Half way there...")
+        # We need to close and reopen for the sensor to cooperate
+        time.sleep(1)
         ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
         print("Done connecting to serial port")
 
         # lines_so_far = 0
         while True:
-            line = ser.readline().decode('ascii')
-            # Uncomment this to print the data for debugging
-            # print(">"+line.replace("\n", ">\n").strip())
-
-            # # Send dummy wifi name & password
-            # if lines_so_far < 2:
-            #     lines_so_far+=1
-            #     ser.write("no_wifi_pls".encode('ascii'))
-            #     continue
-
-            # parse data packets
             try:
-                data = json.loads(line)
-                if "x" in data:
-                    # Crisis Lab X-axis
-                    send_data_to_server("CLX", data["x"])
-                    
-                if "y" in data:
-                    # Crisis Lab Y-axis
-                    send_data_to_server("CLY", data["y"])
-                    
-                if "z" in data:
-                    # Crisis Lab Z-axis
-                    send_data_to_server("CLZ", data["z"])
-                
+                line = ser.readline().decode('ascii')
+                x, y, z = line.strip().split(",")
+                if (len(buffers["x"]) % 10 == 0):
+                    print(x,y,z)
+                buffers["x"].append(x)
+                buffers["y"].append(y)
+                buffers["z"].append(z)
+
+                if len(buffers["x"]) >= 100:
+                    try:
+                        send_data_to_server("x", buffers["x"])
+                        send_data_to_server("y", buffers["y"])
+                        send_data_to_server("z", buffers["z"])
+                    except Exception as err:
+                        print("Error sending data: ",err)
+                        
+                    buffers["x"] = []
+                    buffers["y"] = []
+                    buffers["z"] = []
+
             except Exception as err:
-                pass
+                print("Error reading line: ",err)
+       
             
     except Exception as err:
         print(err)
